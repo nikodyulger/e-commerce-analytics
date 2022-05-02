d7cdefbc8ff65b0ffa58bbac7b64ffe4912cff6a
nikodyulger <nikolasvdulgerov@gmail.com>
2022-05-02 14:10:50 +0200

    App

---
+++ e-commerce-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <SignIn/> -->
+    <SignUp/>
+  </div>
+</template>
+
+<script>
+// import SignIn from './views/SignIn.vue'
+import SignUp from './views/SignUp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // SignIn,
+    SignUp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

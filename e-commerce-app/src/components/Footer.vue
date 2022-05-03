e78c24aeb59104cc9de1ea556901dfa22432882b
nikodyulger <nikolasvdulgerov@gmail.com>
2022-05-03 10:05:50 +0200

    Partials

---
+++ e-commerce-app/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="bg-light fixed-bottom border-0">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col text-center text-dark pt-2">
+          <p>
+            <small>
+              Pet Shop created by Log(ic) for PGyTIC
+            </small>
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+};
+</script>
--- e-commerce-app/src/components/NavBar.vue
+++ e-commerce-app/src/components/NavBar.vue
@@ -1,106 +1,35 @@
 <template>
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <a class="navbar-brand" href="/" onclick="navigateTo('/', event)">
-      Pet Shop
-    </a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarsExampleDefault"
-      aria-controls="navbarsExampleDefault"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div
-      class="collapse navbar-collapse justify-content-end"
-      id="navbarsExampleDefault"
-    >
-      <ul class="navbar-nav">
-        <li class="nav-item dropdown" v-if="false">
-          <a
-            class="nav-link dropdown-toggle d-flex align-items-center"
-            id="dropdown01"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-            &nbsp;Cart&nbsp;
-            <span class="badge badge-pill badge-danger">
-              {{ this.user.shoppingCart.qty }}
-            </span>
-          </a>
-          <div
-            class="dropdown-menu dropdown-menu-right"
-            aria-labelledby="dropdown01"
-          >
-            <a class="dropdown-item" onclick="navigateTo('/cart',event)">
-              Show cart
-            </a>
-            <a class="dropdown-item" onclick="navigateTo('/purchase', event)">
-              Purchase
-            </a>
-          </div>
-        </li>
-
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle d-flex align-items-center"
-            href="#"
-            id="dropdown01"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <i class="far fa-user"></i>
-            &nbsp;User
-          </a>
-          <div
-            class="dropdown-menu dropdown-menu-right"
-            aria-labelledby="dropdown01"
-          >
-            <a
-              class="dropdown-item"
-              href="/signup"
-              v-if="true"
-              onclick="navigateTo('/signup', event)"
-            >
-              Sign Up
-            </a>
-            <a
-              class="dropdown-item"
-              v-if="false"
-              href="/signin"
-              onclick="navigateTo('/signin', event)"
-            >
-              Sign In
-            </a>
-            <a
-              class="dropdown-item"
-              v-if="false"
-              onclick="navigateTo('/profile', event)"
-            >
-              Profile
-            </a>
-            <a
-              class="dropdown-item"
-              v-if="false"
-              onclick="signout_clicked(event)"
-            >
-              Sign Out
-            </a>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </nav>
+  <div>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-brand href="/">Petshop</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse" right></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="/cart">
+          <b-icon icon="cart">
+          </b-icon>
+          Cart
+          </b-nav-item>
+            
+          <b-nav-item-dropdown no-caret>
+            <template #button-content>
+              <b-icon icon="person-circle"></b-icon> User
+            </template>
+            <b-dropdown-item href="/signup">Sign Up</b-dropdown-item>
+            <b-dropdown-item href="/signin">Sign In</b-dropdown-item>
+            <b-dropdown-item href="/profile">Profile</b-dropdown-item>
+            <b-dropdown-item >Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "NavBar",
+};
 </script>
 
 <style>

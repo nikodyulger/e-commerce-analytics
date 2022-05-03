7d1aeb360f83cb008b14394984f3ac6395b17fda
nikodyulger <nikolasvdulgerov@gmail.com>
2022-05-03 10:06:31 +0200

    NotFound View

---
+++ e-commerce-app/src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page-wrap d-flex flex-row align-items-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-12 text-center">
+          <span class="display-1 d-block">404</span>
+          <div class="mb-4 lead">
+            The page you are looking for was not found.
+          </div>
+          <b-link class="btn btn-link" to="/"
+            >Back to Home</b-link
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

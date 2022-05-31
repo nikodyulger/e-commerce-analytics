4184be43515ad94e5400784170876859aec43b2d
nikodyulger <nikolasvdulgerov@gmail.com>
2022-05-31 18:24:57 +0200

    Vista Home traer datos productos

--- e-commerce-app/src/views/Home.vue
+++ e-commerce-app/src/views/Home.vue
@@ -13,13 +13,15 @@
         </div>
       </div>
     </div>
-    <br/>
-    <br/>
+    <br />
+    <br />
     <Footer />
   </div>
 </template>
 
 <script>
+import API from "../services/API";
+
 import { NavBar, Footer, Product } from "@/components";
 
 export default {
@@ -31,69 +33,18 @@ export default {
   },
   data() {
     return {
-      products: [
-        {
-          title: "Turtle Aquarium",
-          category: "Reptiles",
-          url: "../assets/turtleaquarium.jpg",
-          description:
-            "Wide turtle hatch, designed to keep your pet in a space to move freely.",
-          price: 35,
-        },
-        {
-          title: "Bird Cage",
-          category: "Birds",
-          url: "../assets/birdcage.jpg",
-          description: "Perfect birdhouse for your home in white.",
-          price: 30,
-        },
-        {
-          title: "Dog Bed",
-          category: "Dogs",
-          url: "static/dogbed.jpg",
-          description:
-            "Perfect dog mattress to guarantee the rest of the pet. There are several sizes available.",
-          price: 15,
-        },
-        {
-          title: "Dog Toy",
-          category: "Dogs",
-          url: "static/dogtoy.jpg",
-          description: "Puppy toy. Multicolor for your entertainment.",
-          price: 7,
-        },
-        {
-          title: "Cat House",
-          category: "Cats",
-          url: "static/cathouse.jpg",
-          description:
-            "Cat house with a beautiful design that guarantees the rest of your pet.",
-          price: 56,
-        },
-        {
-          title: "Cat Scratcher",
-          category: "Cats",
-          url: "static/catscratcher.jpg",
-          description: "Sturdy scratcher with toy, soft and fun.",
-          price: 18,
-        },
-        {
-          title: "Hamster Cage",
-          category: "Rodents",
-          url: "static/hamstercage.jpg",
-          description:
-            "Comfortable hamster cage so that it can grow up healthy and strong.",
-          price: 16,
-        },
-        {
-          title: "Fish Aquarium",
-          category: "Fishes",
-          url: "static/fishaquarium.jpg",
-          description: "Perfect aquarium for any corner of your house.",
-          price: 23,
-        },
-      ],
+      loading: true,
+      products: [],
     };
+  },
+  beforeMount() {
+    API.getProducts()
+      .then((res) => {
+        this.products = res.data.result.Items;
+        console.log(this.products);
+        this.loading = false;
+      })
+      .catch((err) => console.log(err));
   },
   methods: {},
 };

919bec3f070b4ed13bf6abee8da44f8540750f1d
nikodyulger <nikolasvdulgerov@gmail.com>
2022-06-02 21:03:38 +0200

    Logica store catálogo

--- e-commerce-app/src/store/catalog.js
+++ e-commerce-app/src/store/catalog.js
@@ -1,10 +1,36 @@
-import { defineStore } from 'pinia';
+import { defineStore } from "pinia";
 
-export const useUserStore = defineStore({
-    id:'catalog',
-    state: () => {
-        return {
-            products: []
-        }
+export const useCatalogStore = defineStore({
+  id: "catalog",
+  state: () => {
+    return {
+      catalog: [],
+      cart: [],
+      tax: 21,
+    };
+  },
+  getters: {
+    cartSubtotal() {
+      return this.cart.reduce((acc, p) => acc + p.total, 0);
+    },
+    cartTotal() {
+      return this.cartSubtotal + (this.cartSubtotal * this.tax) / 100;
+    },
+  },
+  actions: {
+    addProductToCart(product) {
+      if (this.cart.find((p) => p.id === product.id)) {
+        const index = this.cart.indexOf(product);
+        this.cart[index]["qty"] += 1;
+        this.cart[index]["total"] += product["price"];
+      } else {
+        product["qty"] = 1;
+        product["total"] = product["price"];
+        this.cart.push(product);
+      }
+    },
+    removeProduct(productId){
+      this.cart = this.cart.filter((p) => p.id !== productId);
     }
-})
+  },
+});

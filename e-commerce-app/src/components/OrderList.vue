136c1d70bfebc40270963dcbb487f5b1cbfc5b2b
nikodyulger <nikolasvdulgerov@gmail.com>
2022-06-02 21:00:56 +0200

    Componente Lista de ordenes

---
+++ e-commerce-app/src/components/OrderList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card mb-3">
+    <div class="card-body">
+      <h2 class="card-title text-left mt-2 mb-4">Order List</h2>
+      <div class="table-responsive">
+        <table class="table table-hover">
+          <thead class="thead-light">
+            <tr>
+              <th
+                scope="col"
+                data-type="date"
+                data-format-string="Do MMMM YYYY"
+              >
+                Date
+              </th>
+              <th scope="col">ID</th>
+              <th scope="col">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="order in user.orders" :key="order.id" >
+              <th class="align-middle">
+                {{ order.date | date}}
+              </th>
+              <td class="align-middle">
+                {{ order.id | orderId }}
+              </td>
+              <td class="align-middle">
+                {{ order.total | currency}}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "OrderList",
+};
+</script>

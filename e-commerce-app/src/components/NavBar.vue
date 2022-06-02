407a4745a1957d95ff72ab07dfa8f03379a9ab2e
nikodyulger <nikolasvdulgerov@gmail.com>
2022-06-02 21:00:43 +0200

    Router link en navbar

--- e-commerce-app/src/components/NavBar.vue
+++ e-commerce-app/src/components/NavBar.vue
@@ -1,41 +1,47 @@
 <template>
   <div>
     <b-navbar type="dark" variant="dark">
-      <b-navbar-brand href="/">Petshop</b-navbar-brand>
+      <b-navbar-brand :to="'/'">Petshop</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse" right></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <b-nav-item href="/cart">
-          <b-icon icon="cart">
-          </b-icon>
-          Cart
+          <b-nav-item :to="'/cart'">
+            <b-icon icon="cart"/>
+            Cart
           </b-nav-item>
-            
+
           <b-nav-item-dropdown no-caret>
             <template #button-content>
               <b-icon icon="person-circle"></b-icon> User
             </template>
-            <b-dropdown-item href="/signup" v-if="!isLogged">Sign Up</b-dropdown-item>
-            <b-dropdown-item href="/signin" v-if="!isLogged">Sign In</b-dropdown-item>
-            <b-dropdown-item href="/profile" v-if="isLogged">Profile</b-dropdown-item>
-            <b-dropdown-item  v-if="isLogged">Sign Out</b-dropdown-item>
+            <b-dropdown-item :to="'/signup'" v-if="!isLogged"
+              >Sign Up</b-dropdown-item
+            >
+            <b-dropdown-item :to="'/signin'" v-if="!isLogged"
+              >Sign In</b-dropdown-item
+            >
+            <b-dropdown-item :to="'/profile'" v-if="isLogged"
+              >Profile</b-dropdown-item
+            >
+            <b-dropdown-item v-if="isLogged" @click="signOut">Sign Out</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
-            {{user}}
-            {{isLogged}}
   </div>
 </template>
 
 <script>
-import { mapState } from 'pinia';
-import { useUserStore } from '../store/user';
+import { mapActions, mapState } from "pinia";
+import { useUserStore } from "../store/user";
 
 export default {
   name: "NavBar",
   computed: {
-    ...mapState(useUserStore, ['isLogged', 'user'])
+    ...mapState(useUserStore, ["isLogged"]),
+  },
+  methods: {
+    ...mapActions(useUserStore, ['signOut'])
   }
 };
 </script>

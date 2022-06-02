07e3914d9d98b6ab32b7bd8ace5c88053fa938a3
nikodyulger <nikolasvdulgerov@gmail.com>
2022-06-02 21:00:00 +0200

    Componente Lista de Ordenes vacia

---
+++ e-commerce-app/src/components/AlertOrder.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card mb-3">
+    <div class="card-body">
+      <h2 class="card-title text-left mt-2 mb-4">Order List</h2>
+      <div class="alert alert-warning" role="alert">
+        <i class="fas fa-exclamation-circle"></i>&nbsp;
+        <span>No orders yet!</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "AlertOrder",
+};
+</script>

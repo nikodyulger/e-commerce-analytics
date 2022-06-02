29bf1b784307eb37edaaf473c9c5300ac484ff7e
nikodyulger <nikolasvdulgerov@gmail.com>
2022-06-02 21:03:27 +0200

    Modulo de componentes

--- e-commerce-app/src/components/index.js
+++ e-commerce-app/src/components/index.js
@@ -1,9 +1,21 @@
-import NavBar from './NavBar.vue'
-import Footer from './Footer.vue'
-import Product from './Product.vue'
+import NavBar from './NavBar.vue';
+import Footer from './Footer.vue';
+import Product from './Product.vue';
+import AlertCart from './AlertCart.vue';
+import CartSummary from './CartSummary.vue';
+import CartTable from './CartTable.vue';
+import ProfileInfo from './ProfileInfo.vue';
+import AlertOrder from './AlertOrder.vue';
+import OrderList from './OrderList.vue';
 
 export {
   NavBar,
   Footer,
-  Product
+  Product,
+  AlertCart,
+  CartSummary,
+  CartTable,
+  ProfileInfo,
+  AlertOrder,
+  OrderList
 }

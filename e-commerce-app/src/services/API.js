2ba6ab78575918677fe31bb6c8a9b35aed848bd9
nikodyulger <nikolasvdulgerov@gmail.com>
2022-06-04 18:32:33 +0200

    Nuevo endpoint API

--- e-commerce-app/src/services/API.js
+++ e-commerce-app/src/services/API.js
@@ -1,14 +1,26 @@
 import api from "../http-common";
 
 class API {
-    getProducts(){
-        return api.get('/products');
-    }
+  getProducts() {
+    return api.get("/products");
+  }
 
-    getUser(username) {
-        const params = new URLSearchParams([['id',username]]);
-        return api.get('/user', {params});
-    }
+  getUser(username) {
+    const params = new URLSearchParams([["id", username]]);
+    return api.get("/user", { params });
+  }
+
+  updateUser(user) {
+    return api.post("/userupdate", { user : user } );
+  }
+
+  sendPurchase(phone, order) {
+    console.log("sms_order", order)
+    return api.post('/sms', {
+      phone: phone,
+      order: order
+    });
+  }
 }
 
 export default new API();
